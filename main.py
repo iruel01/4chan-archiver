82a197becfc918282e69e5e6a63842bff86a6b18
iruel01 <proxy.of.void@gmail.com>
2014-02-11 00:34:24 -0800

    updating main.py

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import os
 current_threads = {}
 greet = "4chan image archiver - Alice (iruel01 on github)"
 help_msg = "type help for usage"
-usage = "add thread <thread url>\nlist threads <board letter(s)>\nremove thread <board letter(s)> <thread number>\nupdate thread <board letter(s)> <thread number>\n\exit"
-
+usage = "add thread <board letter(s)> <thread number>\nlist threads <board letter(s)>\nremove thread <board letter(s)> <thread number>\nupdate thread <board letter(s)> <thread number>\n\exit"
+url_str_main = "boards.4chan/"
+url_str_res = "res/"
 
 def main():
 	print greet
@@ -17,7 +18,10 @@ def main():
 	while input_str != "exit":
 		input_str = prompt()
 
+		if input_str == "help":
+			usage()
 
+		choose_op(input_str)
 
 def usage():
 	print usage
@@ -31,5 +35,19 @@ def prompt():
 def choose_op(input_str):
 	args = input_str.split(' ')
 
-	if args[0] == "add" and args[1] == thread:
+	if args[0] == "add" and args[1] == "thread":
+	elif args[0] == "list" and args[1] == "thread":
+	elif args[0] == "remove" and args[1] == "thread":
+	elif args[0] == "update" and args[1] == "thread":
+
+def add(board_letter, board_number):
+
+def list(board_letter):
+
+def remove(board_letter, board_number):
+
+def update(board_letter, board_number):
+
+
+
 		
