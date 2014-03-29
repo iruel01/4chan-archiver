a23db833b3698a4f0c04917a7738bdd15881acbe
iruel01 <proxy.of.void@gmail.com>
2014-03-28 22:28:08 -0700

    fixed bug with adding threads and calling the download script

--- dta.sh
+++ dta.sh
@@ -1,11 +1,5 @@
-#!/bin/zsh
-
-mkdir $2
-cd $2
-mkdir $3
-cd $3
-wget -O - $1 |
-grep -Eo 'i.4cdn.org/[^"]+' |
-uniq |
-xargs wget
-cd ../../
+  #!/bin/zsh
+  wget -O - $1 |
+  grep -Eo 'i.4cdn.org/[^"]+' |
+  uniq |
+  xargs wget
--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import urllib2
 current_threads = {}
 greet = "4chan image archiver - Alice (iruel01 on github)"
 help_msg = "type help for usage"
-usage = "add thread <board letter(s)> <thread number>\nlist threads <board letter(s)>\nremove thread <board letter(s)> <thread number>\nupdate thread <board letter(s)> <thread number>\n\exit"
-url_str_main = "boards.4chan/"
+usage = "add thread <board letter(s)> <thread number>\nlist threads <board letter(s)>\nremove thread <board letter(s)> <thread number>\nupdate thread <board letter(s)> <thread number>\nexit"
+url_str_main = "http://boards.4chan.org/"
 url_str_res = "res/"
 
 #read in the list from a file
@@ -20,8 +20,8 @@ def update_dictionary():
 def update_list():
 	pass
 
-def usage():
-	print usage
+def usagef():
+	print(usage)
 
 def prompt():
 	input_str = ""
@@ -34,6 +34,7 @@ def choose_op(input_str):
 
 	if args[0] == "add" and args[1] == "thread":
 		add(args[2], args[3])
+		update(args[2], args[3])
 	elif args[0] == "list" and args[1] == "thread":
 		list(args[2])
 	elif args[0] == "remove" and args[1] == "thread":
@@ -41,12 +42,12 @@ def choose_op(input_str):
 	elif args[0] == "update" and args[1] == "thread":
 		update(args[2], args[3])
 	elif args[0] == "help":
-		usage()
+		usagef()
 	else:
-		usage()
+		usagef()
 
 def add(board_letter, board_number):
-	new_string = url_str_main + board_letter + url_str_res + board_number
+	new_string = url_str_main + board_letter + "/" + url_str_res + board_number
 
 def list(board_letter):
 	print "current threads"
@@ -73,14 +74,20 @@ def update(board_letter, board_number):
 		print "thread does not exist"
 		return
 
-	new_string = url_str_main + board_letter + url_str_res + board_number
-	if check_url():
-		call(["./dta.sh", new_string, board_letter, board_number])
+	new_string = url_str_main + board_letter + "/" + url_str_res + board_number
+	if check_url(new_string):
+		print new_string
+		call(["mkdir", board_letter])
+		call(["mkdir", board_letter + "/" + board_number])
+		os.system("cd" + " " + "/" + board_letter + "/" + board_number)
+		os.system("./dta.sh" + " " + new_string)
+		os.system("cd" + " " + "../../")
 	else:
 		print "thread is 404\n removing thread"
 		remove(board_letter, board_number)
 
 def check_url(new_string):
+	return 1
 	req = urllib2.Request(new_string)
 	try:
 		resp = urllib2.urlopen(req)
@@ -101,9 +108,6 @@ def main():
 
 	while input_str != "exit":
 		input_str = prompt()
-
-		if input_str == "help":
-			usage()
 
 		choose_op(input_str)
 
